0e72544ed50234ad23ea39345b8c028b2f62dc8c
EvaIlievska <ilievska_eva@yahoo.com>
2024-02-11 17:13:39 +0100

    vezba53

---
+++ src/Minimum3br.java
@@ -0,0 +1,9 @@
+
+public class Minimum3br {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/n_2.java
@@ -0,0 +1,9 @@
+
+public class n_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
